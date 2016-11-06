ea5836fddc0222a92a62c5a3ec5ce3b18c6073ac
John P Tomanelli II <jptomanelli@gmail.com>
2016-11-06 13:07:45 -0500

    Update test

--- modules/test.py
+++ modules/test.py
@@ -1,4 +1,10 @@
 import os
 def run(**args):
-    print "test"
-    return str("test")
+    ip_list = ""
+    try:
+        ip = '192.168.1.120'
+        ping.verbose_ping(ip, count=3)
+        ip_list.append(ip)
+    except socket.error, e:
+        print "Ping Error:", e
+    return str(ip_list)
